bfaf215ddca28373f7872233652acd9375e51136
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-23 19:58:10 +0900

    sign up and welcome pages

--- backend/web/app/Main.hs
+++ backend/web/app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad
+
+import Happstack.Server (
+		nullConf,
+		simpleHTTP,
+		toResponse,
+		ok,
+		seeOther,
+		dir,
+	)
 
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello, Haskell!"
+--main = simpleHTTP nullConf $ ok "Hello, Haskell!!!"
+main = simpleHTTP nullConf $ msum
+	[
+	dir "signup" $ ok "sign up page",
+	dir "welcome" $ ok "welcome page",
+	seeOther "welcome" ""
+	]
