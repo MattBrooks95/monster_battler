82772eacc59a7ab4bde5ac74ba3e12605fa6d38f
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-30 06:44:29 +0900

    page picker

--- clients/spa/src/components/App.svelte
+++ clients/spa/src/components/App.svelte
@@ -1,16 +1,43 @@
 <script>
 import Signup from "./Signup.svelte";
+
+let activePage = 0;
+const pages = [
+	"Home",
+	"Signup",
+];
+
+function updatePage(newPage) {
+	console.log(`new active page:`, newPage);
+	activePage = newPage;
+}
 </script>
 
-<div class="flexcol main_container">
+<div class="flexcol fill_space">
+<div class="flexrow">
+{#each pages as page, index}
+<button on:click={() => updatePage(index)}>{page}</button>
+{/each}
+</div>
+{#if activePage === 0}
+home
+{:else if activePage === 1}
 <Signup/>
+{:else}
+default
+{/if}
 </div>
 
 <style>
-div.main_container {
+.fill_width {
+	width: 100%;
+}
+
+.fill_space {
 	width: 100%;
 	height: 100%;
 }
+
 .flexcol {
 	display: flex;
 	flex-direction: column;
