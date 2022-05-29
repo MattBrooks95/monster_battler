2a5181a22a0332c5409195aec9f5e0721783fed9
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-29 16:35:47 +0900

    i needed toResponse!!!
    https://stackoverflow.com/questions/48738375/whats-the-syntax-of-using-multiple-handlers-in-happstack

--- backend/web/app/Main.hs
+++ backend/web/app/Main.hs
@@ -19,8 +19,8 @@ main :: IO ()
 --main = simpleHTTP nullConf $ ok "Hello, Haskell!!!"
 main = simpleHTTP nullConf $ msum
 	[
-	dir "signup" $ serveFile (asContentType "text/html") "static/index.html"
-	--dir "account" $ ok "account page",
+	dir "signup" $ serveFile (asContentType "text/html") "static/index.html",
+	dir "account" $ ok $ toResponse "account page"
 	--dir "welcome" $ ok "welcome page",
 	--dir "api" $ ok "api endpoint",
 	--seeOther "welcome" ""
