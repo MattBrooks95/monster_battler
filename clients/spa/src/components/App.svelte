367e9d03e6e46590920ccea0a8b03ec32204881d
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-30 06:13:22 +0900

    forgot to include the stylesheet that svelte creates

--- clients/spa/src/components/App.svelte
+++ clients/spa/src/components/App.svelte
@@ -2,6 +2,19 @@
 import Signup from "./Signup.svelte";
 </script>
 
-<div>
+<div class="flexcol main_container">
 <Signup/>
 </div>
+
+<style>
+div.main_container {
+	width: 100%;
+	height: 100%;
+}
+.flexcol {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
+}
+</style>
