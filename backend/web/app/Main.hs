c8974cd085731f9050b20f82d324415a9d881f32
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-31 21:24:25 +0900

    this setup is interesting: I'm trying to learn how to separate these handlers out into separate functions
    but I'm having a hard time with the type system because I don't
    understand monads and the return keyword

--- backend/web/app/Main.hs
+++ backend/web/app/Main.hs
@@ -16,19 +16,36 @@ import Happstack.Server (
 		serveDirectory,
 		Browsing(..),
 		Method(..),
-		method
+		method,
+		ServerPartT,
+		ToMessage
 	)
 
 main :: IO ()
 --main = simpleHTTP nullConf $ ok "Hello, Haskell!!!"
 main = simpleHTTP nullConf $ msum
 	[
-	do dir "signup" $ serveFile (asContentType "text/html") "static/index.html",
-	do dir "account" $ ok $ toResponse "account page",
-	do dir "welcome" $ ok $ toResponse "welcome page",
-	--dir "api" $ ok $ toResponse "api endpoint",
-	do dir "api" $ dir "person" (do method PUT
-					ok $ toResponse "api/person"),
-	do dir "static" $ serveDirectory DisableBrowsing [] "static"
-	--seeOther "welcome" ""
+	dir "signup" $ serveFile (asContentType "text/html") "static/index.html",
+	dir "static" $ serveDirectory DisableBrowsing [] "static",
+	dir "api" api
+	--do dir "account" $ ok $ toResponse "account page",
+	--do dir "welcome" $ ok $ toResponse "welcome page",
+	----dir "api" $ ok $ toResponse "api endpoint",
+	----do dir "api" $ dir "person" (do method PUT
+	----				ok $ toResponse "api/person"),
+	--(do dir "api" $ apiRouting),
+	--do dir "static" $ serveDirectory DisableBrowsing [] "static"
+	----seeOther "welcome" ""
+	]
+
+--apiRouting :: (ToMessage a) => ServerPartT IO a
+--apiRouting = do
+--	dir "person" (do method PUT ok $ toResponse "api/person")
+
+--personHandler :: ServerPartT IO String
+--personHandler = return (method PUT (return ok $ toResponse "api/person"))
+
+api = msum
+	[
+		dir "person" $ ok $ toResponse "api/person"
 	]
