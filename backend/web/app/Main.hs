0a0c028dd543d6a194095fb813caf6a6718f05bf
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-06-02 22:43:51 +0900

    mystery solved -> I hate languages that have significant whitespace

--- backend/web/app/Main.hs
+++ backend/web/app/Main.hs
@@ -67,9 +67,17 @@ person = msum
 		--but for some reason, do notation did not work when I tried it
 		--if it was because of whitespace or indentation, that's just silly
 		--this is the same as the happstack guide, doesn't work because "Couldn't match type ‘()’ with ‘Response -> ServerPartT IO Response’"
+		--the reason this does not work is because the 'm' in method (the first line of the do block) and the '(' or 'r' on the second line do not line-up
+		--they have to be on exactly the same column
 		--do method PUT
 		--	(return (ok $ toResponse "api/person put"))
+		--like this
+		do
+			method PUT
+			ok $ toResponse "api/person put"
+
+
 		--this works and I think this syntax is nicer, but why does the do block not work?
 		--the thing1 >> thing2 operator does thing1, ignores any return value it might have had, and then returns the result of thing2
-		method PUT >> (ok $ toResponse "api/person put")
+		--method PUT >> (ok $ toResponse "api/person put")
 	]
