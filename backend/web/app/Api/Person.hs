c7021a0072c3d1a1576286e8854ffc6abeb2da72
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-06-08 20:46:49 +0900

    okay this is kind of interesting

--- backend/web/app/Api/Person.hs
+++ backend/web/app/Api/Person.hs
@@ -1,30 +1,31 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Api.Person where
 
 import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Logger
-import Database.Persist.Postgresql
+import Control.Monad.Logger    (runStderrLoggingT)
 
 import GHC.Generics
+
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Maybe
+
+import Database.Persist
+import Database.Persist.Postgresql
+import Database.Persist.TH
 
 import qualified Data.ByteString.Lazy as LB
 
@@ -51,25 +52,28 @@ data PersonJson = PersonJson { name::String } deriving (Show, Generic, ToJSON, F
 -- TODO you copy and pasted this
 connStr = "host=localhost dbname=monster_battler user=monster_battler password=monster_battler port=5432"
 
+--runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+--flip runSqlPersistMPool pool $ do
+
 person :: ServerPartT IO Response
-person = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
-	flip runSqlPersistMPool pool $ do
-		msum [
-			do
-				method PUT
-				request <- askRq
-				body <- liftIO $ takeRequestBody request
-				let bodyAsJson = getBodyAsJson body
-				case bodyAsJson of
-					Nothing -> ok $ toResponse "request body decode failure"
-					Just bodyAsJson -> ok $ toResponse $ show bodyAsJson,
-			do
-				method GET
-				ok $ toResponse "api/person get",
-			do
-				method DELETE
-				ok $ toResponse "api/person delete"
-		]
+person = msum [
+				do
+					method PUT
+					request <- askRq
+					body <- liftIO $ takeRequestBody request
+					let bodyAsJson = getBodyAsJson body
+					case bodyAsJson of
+						Nothing -> ok $ toResponse ("request body decode failure":: String)
+						Just bodyAsJson -> do
+							liftIO $ insertPerson bodyAsJson
+							ok $ toResponse $ show bodyAsJson
+				--do
+				--	method GET
+				--	ok $ toResponse "api/person get",
+				--do
+				--	method DELETE
+				--	ok $ toResponse "api/person delete"
+			]
 
 getBodyAsJson :: Maybe RqBody -> Maybe PersonJson
 getBodyAsJson body =
@@ -78,3 +82,6 @@ getBodyAsJson body =
 			let fromJson = decode (unBody bodySuccess) :: Maybe PersonJson
 			fromJson
 		Nothing -> Nothing
+
+insertPerson :: PersonJson -> IO ()
+insertPerson person = return ()
