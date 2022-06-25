60e087932a9b956b5e42cc49b828c7d6ae64b1fd
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-06-25 15:15:16 +0900

    was able to print the list of registered people to the screen

--- clients/spa/src/components/Admin.svelte
+++ clients/spa/src/components/Admin.svelte
@@ -1,23 +1,26 @@
 <script>
 import { onMount } from 'svelte';
 
+let people = [];
+
 onMount(async () => {
 	const response = await fetch('api/person', {
 		method: 'GET'
 	});
 	console.log(response);
-	//const respText = await response.text();
-	//console.log(respText, typeof respText);
-	//console.log(JSON.parse(respText));
 
 	const respJson = await response.json();
 	console.log(respJson);
+	people = respJson;
 
 });
 
 </script>
 
 <div>User Accounts</div>
+{#each people as person}
+<div>Name:{person.personName}</div>
+{/each}
 
 <style>
 </style>
