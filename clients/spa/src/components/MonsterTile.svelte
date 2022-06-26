32cca0896f76617ba0ebb34cb18b85caa8d2ea88
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-06-27 07:36:16 +0900

    working on a dynamic list of elements to display the monsters, with dummy data

---
+++ clients/spa/src/components/MonsterTile.svelte
@@ -0,0 +1,37 @@
+<script>
+export let monster;
+</script>
+
+<div class="monsterTile">
+	<div>HP</div>
+	<div>SPE</div>
+	<div>ATT</div>
+	<div>DEF</div>
+	<div>SPA</div>
+	<div>SPD</div>
+	<div>ABIL</div>
+	<div>
+		<span>Types</span>
+		<div class="flexRow">
+			{#each monster.types as type}
+			<div>{type}</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+.monsterTile {
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	align-items: center;
+}
+
+.flexRow {
+	display: flex;
+	flex-direction: row;
+	gap: 8px;
+}
+</style>
+
--- clients/spa/src/components/Monsters.svelte
+++ clients/spa/src/components/Monsters.svelte
@@ -1,7 +1,51 @@
 <script>
+import { onMount } from 'svelte';
+
+import MonsterTile from "./MonsterTile.svelte";
+
+let monsters = [];
+onMount(async () => {
+	monsters = [
+		{ types: ["Astral", "Fire"] },
+		{ types: ["Poison", "Beast"] },
+		{ types: ["Fighting"] },
+		{ types: ["Primal", "Flying"] },
+		{ types: ["Water"], }
+	];
+});
+
+//onMount(async () => {
+//	const response = await fetch('api/monster', {
+//		method: 'GET'
+//	});
+//
+//	const respJson = await response.json();
+//	monsters = respJson
+//
+//});
+
 </script>
 
-Monsters List
+<div>Monsters List</div>
+<div class="monsterGrid">
+	{#each monsters as monster}
+	<MonsterTile monster={monster} />
+	{/each}
+</div>
 
 <style>
+.monsterGrid {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 8px;
+}
+
+/* you cannot just do .myClass > *, if the child elements are components,
+* use :global
+* https://stackoverflow.com/questions/56988717/how-to-target-a-component-in-svelte-with-css
+*/
+.monsterGrid > :global(*) {
+	min-width: 300px;
+}
 </style>
