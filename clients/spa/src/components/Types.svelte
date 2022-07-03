0b6795cb38a1544f8efdbbeb2cf0fc2f2eabfbdf
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-07-03 13:35:56 +0900

    was able to also return the ids of the types from haskell, so that the client can desplay the matchup information as the type names instead of the type keys

--- clients/spa/src/components/Types.svelte
+++ clients/spa/src/components/Types.svelte
@@ -3,6 +3,8 @@ import { onMount } from "svelte";
 
 let types = [];
 
+let typeIdMap = {};
+
 onMount(async () => {
 	const response = await fetch('api/types', {
 		method: 'GET'
@@ -10,13 +12,36 @@ onMount(async () => {
 	console.log(response);
 
 	const respJson = await response.json();
-	console.log(respJson);
-	types = respJson;
+	//console.log(respJson);
+	//make a map from type id to type name, for presentation
+	respJson.forEach(typeIdVal => {
+		const key = typeIdVal[0];
+		const typeName = typeIdVal[1].typeName;
+		typeIdMap[key] = typeName;
+	});
+
+	//helper for looking up types
+	const lookupType = (typeId) => typeIdMap[typeId];
+
+	types = respJson.map(typeIdVal => {
+		const typeInfo = typeIdVal[1];
+		return {
+			type: typeInfo.typeName,
+			strongAttacks: typeInfo.typeStrongAttacks.map(lookupType),
+			resists: typeInfo.typeResists.map(lookupType),
+		}
+	});
+	console.log({ typeIdMap, types });
+	//curious to see when this happens, isn't being called still, even if I display another component
+	//which probably means that the components are just being hidden when a different tab is selected
+	return () => {
+		console.log('types component destroyed');
+	};
 });
 </script>
 
 {#each types as type}
-<div>type:{type.typeName} strongAgainst:{type.typeStrongAttacks.join(' ')} resists:{type.typeResists.join(' ')}</div>
+<div>type:{type.type} strongAgainst:{type.strongAttacks.join(' ')} resists:{type.resists.join(' ')}</div>
 {/each}
 
 <style>
