73d42f259ab68e29b2d2bd70a43f158d4528def1
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-08-02 18:29:25 +0900

    ending this branch without changing much, think I started down the wrong path

--- backend/monster-battler-game/src/Lib.hs
+++ backend/monster-battler-game/src/Lib.hs
@@ -1,9 +1,9 @@
 module Lib where
 import Game
 
+-- TODO I don't think I need this. I should just test from the web client or in ghci
 testGame :: IO ()
 testGame = do
 	print "testGame"
 	let dummyGame = Game { player_teams = [], battle_mode = FreeForAll }
 	print dummyGame
-	print $ runGame dummyGame
